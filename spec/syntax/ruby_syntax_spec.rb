0ba6c0491c331915b9c06a9128e429ab690a6df3
Kohei Suzuki <eagletmt@gmail.com>
2014-02-19 13:36:54 +0900

    Don't put check messages

--- spec/syntax/ruby_syntax_spec.rb
+++ spec/syntax/ruby_syntax_spec.rb
@@ -6,7 +6,6 @@ describe 'Ruby files' do
 
   it 'has no syntax errors or warnings' do
     ruby_files.each do |ruby_file|
-      puts "Check #{ruby_file}"
       check_output, status = Open3.capture2e('ruby', '-wc', ruby_file)
       expect(check_output.chomp).to eq('Syntax OK')
       expect(status.exitstatus).to eq(0)
