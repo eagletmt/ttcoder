867e93ea0f7ebfc2ca7a3adaf232e0cf5f62fc71
Kohei Suzuki <eagletmt@gmail.com>
2014-02-21 04:45:56 +0900

    More appropriate syntax check

--- spec/syntax/ruby_syntax_spec.rb
+++ spec/syntax/ruby_syntax_spec.rb
@@ -1,53 +1,23 @@
 require 'spec_helper'
-require 'ripper'
-
-class WarningParser < Ripper
-  attr_reader :messages
-
-  def initialize(*)
-    super
-    @messages = []
-  end
-
-  private
-
-  def warn(fmt, *args)
-    log(:warn, fmt, *args)
-    super
-  end
-
-  def warning(fmt, *args)
-    log(:warning, fmt, *args)
-    super
-  end
-
-  def compile_error(fmt, *args)
-    log(:compile_error, fmt, *args)
-    super
-  end
-
-  def log(tag, fmt, *args)
-    @messages << sprintf("%s:%d:%d: %s: #{fmt}", filename, lineno, column, tag, *args)
-  end
-end
 
 describe 'Ruby files' do
   let(:ruby_files) { find_files(Rails.root, '.rb') }
+  let(:stderr) { StringIO.new }
 
   around do |example|
     verbose = $VERBOSE
     $VERBOSE = true
+    err = $stderr
+    $stderr = stderr
     example.run
+    $stderr = err
     $VERBOSE = verbose
   end
 
   it 'has no syntax errors or warnings' do
     ruby_files.each do |ruby_file|
-      File.open(ruby_file) do |f|
-        parser = WarningParser.new(f, ruby_file)
-        parser.parse
-        expect(parser.messages).to eq([])
-      end
+      RubyVM::InstructionSequence.compile_file(ruby_file)
     end
+    expect(stderr.string).to eq('')
   end
 end
