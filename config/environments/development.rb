fc81208eb069e447682fb20e47348163adc4c494
Kohei Suzuki <eagletmt@gmail.com>
2014-02-22 01:59:57 +0900

    Change ActionMailer delivery method to :file in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,7 +35,5 @@ Rails.application.configure do
   # Raises error for missing translations
   config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = Rails.application.secrets.smtp
+  config.action_mailer.delivery_method = :file
 end
