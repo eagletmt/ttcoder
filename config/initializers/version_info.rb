566197f266b5917cfe962a5ffb7ffa53a9ae939d
Kohei Suzuki <eagletmt@gmail.com>
2014-03-20 18:12:35 +0900

    Link to commit page on GitHub

--- config/initializers/version_info.rb
+++ config/initializers/version_info.rb
@@ -2,7 +2,8 @@ Rails.application.config.current_version =
   if Rails.env.production?
     rev = Rails.root.join('REVISION').read.chomp
     rel = File.basename(Dir.pwd)
-    "#{rev} / #{rel}"
+    link = "https://github.com/eagletmt/ttcoder/commit/#{rev}"
+    %Q{<a href="#{link}">#{rev}</a> / #{rel}}.html_safe
   else
     rev = `git rev-parse --short master`.chomp
     rel = Time.now.strftime('%Y%m%d%H%M%S')
