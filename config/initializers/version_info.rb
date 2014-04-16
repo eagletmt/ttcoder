23b990b626dc32fc72b6418aaa015e6d83dfbcdc
Kohei Suzuki <eagletmt@gmail.com>
2014-04-16 23:32:22 +0900

    Tweak version info style in response to capistrano 3.2 change

--- config/initializers/version_info.rb
+++ config/initializers/version_info.rb
@@ -3,9 +3,9 @@ Rails.application.config.current_version =
     rev = Rails.root.join('REVISION').read.chomp
     rel = File.basename(Dir.pwd)
     link = "https://github.com/eagletmt/ttcoder/commit/#{rev}"
-    %Q{<a href="#{link}">#{rev}</a> / #{rel}}.html_safe
+    %Q{<a href="#{link}">#{rev}</a><br>#{rel}}.html_safe
   else
-    rev = `git rev-parse --short master`.chomp
+    rev = `git rev-parse master`.chomp
     rel = Time.now.strftime('%Y%m%d%H%M%S')
-    "#{rev} / #{rel}"
+    "#{rev}<br>#{rel}".html_safe
   end
