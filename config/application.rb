3afc162be73ffc19ac4239c2023914c49dcf463a
Kohei Suzuki <eagletmt@gmail.com>
2014-08-23 23:22:10 +0900

    rake rails:update

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,9 @@ module Ttcoder
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.i18n.enforce_available_locales = true
+
+    # For not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
 
     config.active_record.timestamped_migrations = false
   end
