224ddbe8cb7c060da9e34ac5d47b10b1c67a9200
Kohei Suzuki <eagletmt@gmail.com>
2014-12-08 23:30:36 +0900

    Disable AlertMailer in web app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,32 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  around_action :fluent_logger
-  def fluent_logger
-    r = yield
-    Fluent::Logger.post('access', {
-      remote_ip: request.remote_ip,
-      method: request.method,
-      path: request.fullpath,
-      user_agent: request.user_agent,
-      controller: params[:controller],
-      action: params[:action],
-      user_id: session[:user_id],
-      status: response.status,
-    })
-    r
-  rescue Exception => e
-    Fluent::Logger.post('controller_exception', {
-      class: e.class.name,
-      message: e.message,
-      backtrace: Rails.backtrace_cleaner.clean(e.backtrace),
-    })
-    unless e.is_a?(ActiveRecord::RecordNotFound)
-      AlertMailer.controller_exception(e).deliver
-    end
-    raise e
-  end
-
   before_action :set_leftbar_contests
   def set_leftbar_contests
     @leftbar_contests = Contest.all.limit(30)
