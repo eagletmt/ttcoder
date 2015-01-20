107e50afe4d7e0c3afd52d9b9324c78f74c7ffac
Kohei Suzuki <eagletmt@gmail.com>
2015-01-21 01:05:42 +0900

    Assume PostgreSQL

--- db/migrate/006_make_username_case_insensitve.rb
+++ db/migrate/006_make_username_case_insensitve.rb
@@ -8,14 +8,7 @@ class MakeUsernameCaseInsensitve < ActiveRecord::Migration
     TARGETS.each do |table_name, column_name|
       remove_index table_name, column_name
 
-      case ActiveRecord::Base.connection.adapter_name
-      when 'SQLite'
-        execute "CREATE INDEX #{quote_column_name(index_name(table_name, column: column_name))} ON #{quote_table_name(table_name)} (#{quote_column_name(column_name)} COLLATE NOCASE)"
-      when 'PostgreSQL'
-        execute "CREATE INDEX #{quote_column_name(index_name(table_name, column: column_name))} ON #{quote_table_name(table_name)} (lower(#{quote_column_name(column_name)}))"
-      else
-        fail 'Unsupported database'
-      end
+      execute "CREATE INDEX #{quote_column_name(index_name(table_name, column: column_name))} ON #{quote_table_name(table_name)} (lower(#{quote_column_name(column_name)}))"
     end
   end
 
