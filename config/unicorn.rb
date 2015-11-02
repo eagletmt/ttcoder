43c49786618225e06504901395eaf500d33ac844
Kohei Suzuki <eagletmt@gmail.com>
2015-11-03 03:47:17 +0900

    Enable systemd socket activation with Unicorn 5.0.0

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,8 +3,6 @@ worker_processes 2
 app_path = File.expand_path('../..', __FILE__)
 
 working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-listen '/tmp/ttcoder.sock'
 
 stderr_path "#{app_path}/log/unicorn-stderr.log"
 stdout_path "#{app_path}/log/unicorn-stdout.log"
